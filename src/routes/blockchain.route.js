7f4223e41dca9fd70ad4af777b4db8ca6b47d4e8
Neil Bennett <neil.bennett876@Neils-MacBook-Air.local>
2022-05-06 05:14:08 -0400

    added try catch

--- src/routes/blockchain.route.js
+++ src/routes/blockchain.route.js
@@ -5,32 +5,57 @@ export const blockchainRouter = Router()
 
 blockchainRouter.post('/blockchain', async (req, res) => {
     const blockchain = req.body //defining the body of the message to be sent
+try {
     const id = await createBlockchain(blockchain)
     res.send(id.toString())
-})
 
+    } catch (error) {
+    res.status(500).send(err)
+    }
+
+})
 
 blockchainRouter.get('/blockchain/', async (req, res) => {
     const id = await getBlockchains()
-    res.status(200).send(id)
+    try {
+        res.status(200).send(id)
+        
+    } catch (error) {
+        res.status(500).send(err)
+    }
 })
 
 blockchainRouter.get('/blockchain/:name', async (req, res) => {
     const blockchain = req.params.name
-    const id = await getBlockchain(blockchain)
-    res.status(200).send(id)
+    try {
+        const id = await getBlockchain(blockchain)
+        res.status(200).send(id)
+        
+    } catch (error) {
+        res.status(500).send(err)
+    }
 })
 
 
 blockchainRouter.patch('/blockchain/:name', async (req, res) => {
     const blockchainName = req.params.name
     const updatedBlockchain = req.body
-    await updateBlockchain(blockchainName, updatedBlockchain)
-    res.status(200).send({message: "Updated Successfully"})
+    try {
+        await updateBlockchain(blockchainName, updatedBlockchain)
+        res.status(200).send({message: "Updated Successfully"})
+        
+    } catch (error) {
+        res.status(500).send(err)
+    }
 })
 
 blockchainRouter.delete('/blockchain/:name', async (req, res) => {
     const id = req.params.name
-    await deleteBlockchain(id)
-    res.status(200).send("Successfully deleted")
+    try {
+        await deleteBlockchain(id)
+        res.status(200).send("Successfully deleted")
+        
+    } catch (error) {
+        res.status(500).send(err)
+    }
 })
